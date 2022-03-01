394a4982f0be4abc53072b95828562fae8fb3eea
hezardastan <amirphl4@gmail.com>
2022-03-01 15:53:43 +0330

    Add exercises of loop and switch

---
+++ golang/10_for-loop/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for (i := 0; i < 20; i++) { // invalid
+	for i := 110; i < 140; i++ { // TODO What is happening for 132?
+		fmt.Println(i, 'i', string(i), "---", []byte(string(i)), "---", []int32(string(i)))
+	}
+
+	fmt.Println("\n---------\n")
+
+	i := 10
+	for i < 14 {
+		if i == 12 {
+			break
+		}
+		for j := 10; j < 14; j++ {
+			if j == 11 {
+				continue
+			}
+			fmt.Printf("%d * %d = %d ", i, j, i*j)
+		}
+
+		fmt.Println("")
+		i++
+	}
+
+	fmt.Println("\n---------\n")
+
+	for {
+		if i == 20 {
+			break
+		}
+
+		fmt.Printf("%d ", i)
+		i++
+	}
+
+	fmt.Println("\n---------\n")
+	fmt.Printf("\n%T %T\n", []byte(string(i)), []int32(string(i)))
+
+	// cannot convert "i" (type untyped string) to type rune
+	// fmt.Println(rune("i"))
+	fmt.Println("\n---------\n")
+	fmt.Println([]byte("سلام"), []int32("سلام"))
+}
---
+++ golang/11_switch-statements/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type food struct {
+	name string
+	rate int
+}
+
+func switchOnType(x interface{}) {
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case food:
+		fmt.Println("food")
+	default:
+		fmt.Println("unknown type")
+	}
+}
+
+func main() {
+	i := 10
+	switch {
+	case i == 11, i == 10:
+		fmt.Println("it is 11 or 10")
+		fallthrough
+	case i%2 == 0:
+		fmt.Println("it is even")
+		fallthrough
+	case i%2 == 1:
+		fmt.Println("it is odd")
+		fallthrough
+	case i%5 == 0:
+		fmt.Println("it is multiply of 5")
+	case i%10 == 0:
+		fmt.Println("it is multiply of 10")
+		fallthrough
+	case i%1 == 0:
+		fmt.Println("it is multiply of 1 :))")
+	case i%3 == 0:
+		fmt.Println("it is multiply of 3")
+	case i%10 == 0:
+		fmt.Println("it is multiply of 10 again")
+	default:
+		fmt.Println("it is the default case")
+	}
+
+	fmt.Println("\n---\n")
+
+	y := food{"pizaa", 8}
+	switchOnType(y)
+	switchOnType(y.name)
+	switchOnType(y.rate)
+	switchOnType(nil)
+}
