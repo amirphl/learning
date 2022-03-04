daf9ebb24da1d4d4ae7c03c822cd74f2fe883208
hezardastan <amirphl4@gmail.com>
2022-03-04 23:38:18 +0330

    Add map examples

---
+++ golang/19_map/main.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+func nilMap() {
+	// There is no function like `append` for map.
+
+	// nil map
+
+	var a map[int]string
+	fmt.Printf("a: %v\n", a)
+	fmt.Printf("a == nil: %v\n", a == nil)
+	// a[5] = "salam" // assignment to entry in nil map
+	delete(a, 5) // a is nil but it still works
+	fmt.Println("----------------------")
+
+	// empty map
+
+	var b map[int]int = map[int]int{}
+	fmt.Printf("b: %v\n", b)
+	fmt.Printf("b == nil: %v\n", b == nil)
+	fmt.Printf("***** (default value test) b[5]: %v\n", b[5])
+	b[5] = 5
+	fmt.Printf("(after adding 5:5) b[5]: %v\n", b[5])
+	fmt.Printf("(after adding 5:5) b: %v\n", b)
+	b[5] = 6
+	fmt.Printf("(after adding 5:6) b: %v\n", b)
+	delete(b, 5)
+	fmt.Printf("(after deleting key 5) b: %v\n", b)
+	delete(b, 5)
+	fmt.Printf("(after deleting key 5 again) b: %v\n", b)
+	fmt.Println("----------------------")
+
+	// composite map
+
+	var c = map[float64]float64{
+		4.6:   -1,
+		100.5: 0.6,
+		0:     2,
+	}
+	fmt.Printf("address of c: %p\n", &c)
+	fmt.Println("----------------------")
+
+	// d := map[rune]int16 // expected expression
+	d := map[rune]int16{}
+	fmt.Printf("d: %v\n", d)
+	fmt.Printf("***** d[3]: %v\n", d[3])
+	fmt.Println("----------------------")
+
+	// create map using `make`
+
+	e := make(map[int32]bool, 5)
+	fmt.Printf("e: %v\n", e)
+	val, exists := e[100]
+	fmt.Printf("e{100]: val, exists: %v, %v\n", val, exists) // very important: Although the map doesn't contain key 100, it returns false as the default value (the value type is bool).
+	// another note: we didn't use var while it returns default value.
+	fmt.Println("----------------------")
+
+	// map capacity
+	// Most of the time, setting initial capacity is not necessary. Golang can handle it.
+
+	f := make(map[int8]int8, 3) // initial capacity // It should be tuned well!
+	for i := 0; i < 10; i++ {
+		f[int8(i+11)] = int8(i)
+		// fmt.Printf("cap(f): %v\n", cap(f)) // invalid argument f (type map[int8]int8) for cap
+		fmt.Printf("f, len(f): %v   %v\n", f, len(f))
+	}
+	fmt.Println("----------------------")
+
+	// fetching value from the map
+
+	value, ex := f[0]
+	value2 := f[0]
+	fmt.Printf("value, ex, value2: %v  %v  %v\n", value, ex, value2)
+	fmt.Println("----------------------")
+
+	// iterate over map
+
+	fmt.Println("iterating over f:")
+	for k, value := range f { // variable shadowing :))
+		fmt.Printf("key, value: %v    %v\n", k, value)
+	}
+	fmt.Println("----------------------")
+}
+
+func main() {
+	nilMap()
+}
