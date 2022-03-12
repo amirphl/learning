20773dd0b43aa1a7b2f4648dc0e3e44a9a05f953
root <amirphl4@gmail.com>
2022-03-12 20:10:37 +0330

    Finish goroutines

--- golang/22_goroutines/main.go
+++ golang/22_goroutines/main.go
@@ -1139,6 +1139,41 @@ func incrementorChallengeSolution() {
 	fmt.Println(count)
 }
 
+func what() {
+	m := map[int]int{}
+	m[4] = 7
+	m[3] = 87
+	m[72] = 19
+
+	ch := make(chan int)
+
+	// for closing ch
+	ch2 := make(chan int)
+	go func() {
+		var i int
+		for n := range ch2 {
+			i += n
+			if i == len(m) {
+				close(ch)
+			}
+		}
+	}()
+
+	go func() {
+		for _, v := range m {
+			ch <- v
+			ch2 <- 1
+		}
+	}()
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+
+	// good housekeeping
+	close(ch2)
+}
+
 func main() {
 	// noGo()
 	// noWait()
@@ -1182,4 +1217,5 @@ func main() {
 	// deadLockChallengeSolution()
 	// incrementorChallenge()
 	// incrementorChallengeSolution()
+	// what()
 }
