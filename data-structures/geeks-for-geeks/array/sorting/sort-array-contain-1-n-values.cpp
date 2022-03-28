878fec55772ff929e588259101bcaabd054cd5be
amirphl <amirphl4@gmail.com>
2022-03-28 12:37:34 +0430

    sort-array-contain-1-n-value

---
+++ data-structures/geeks-for-geeks/array/sorting/sort-array-contain-1-n-values.cpp
@@ -0,0 +1,28 @@
+// https://www.geeksforgeeks.org/sort-array-contain-1-n-values/
+
+#include<algorithm>
+#include<iostream>
+
+// time: O(n), memory: O(1)
+void sort(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+}
+
+void print(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+}
+
+int main() {
+	int arr[] = { 10, 7, 9, 2, 8, 3, 5, 4, 6, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	print(arr, n);
+	sort(arr, n);
+	print(arr, n);
+	return 0;
+}
