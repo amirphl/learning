5bccf86779dd9b6e06644ab082653b00bf415894
amirphl <amirphl4@gmail.com>
2022-03-30 13:42:52 +0430

    write-a-c-function-to-print-the-middle-of-the-linked-list

---
+++ data-structures/geeks-for-geeks/linked-list/write-a-c-function-to-print-the-middle-of-the-linked-list.cpp
@@ -0,0 +1,86 @@
+// https://www.geeksforgeeks.org/write-a-c-function-to-print-the-middle-of-the-linked-list/
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    // time: O(n), memory: O(1)
+    int getMiddle(Node *head)
+    {
+        Node* t = head;
+        Node* u = head;
+        while(u != NULL) {
+            u = u -> next;
+            if (u == NULL)
+                break;
+            u = u -> next;
+            t = t -> next;
+        }
+        return t -> data;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
