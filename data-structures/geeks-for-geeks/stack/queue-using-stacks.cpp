2733054d0547b963faa2ac7a19f3d902b632f92c
amirphl <amirphl4@gmail.com>
2022-03-31 01:58:30 +0430

    queue-using-stacks

---
+++ data-structures/geeks-for-geeks/stack/queue-using-stacks.cpp
@@ -0,0 +1,64 @@
+// https://www.geeksforgeeks.org/queue-using-stacks/
+// Implemented with an explicit Stack and the Stack of the memory.
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> s;
+public:
+    // time: O(n), memory: O(1)
+    void enqueue(int x) {
+        if (s.empty())
+            s.push(x);
+        else {
+            int t = s.top();
+            s.pop();
+            enqueue(x);
+            s.push(t);
+        }
+    }
+
+    // time: O(1), memory: O(1)
+    int dequeue() {
+        int t = s.top();
+        s.pop();
+        return t;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+  // } Driver Code Ends
+
