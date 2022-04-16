787736ba7eaeb9dd8e6f06f61110729f965ef902
amirphl <amirphl4@gmail.com>
2022-04-16 16:11:38 +0430

    modify-binary-tree-get-preorder-traversal-using-right-pointers

---
+++ data-structures/geeks-for-geeks/binary-tree/traversal/modify-binary-tree-get-preorder-traversal-using-right-pointers.cpp
@@ -0,0 +1,55 @@
+// https://www.geeksforgeeks.org/modify-binary-tree-get-preorder-traversal-using-right-pointers/
+
+#include<algorithm>
+#include<iostream>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = new struct Node;
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
+// time: O(V*V)?, memory: O(V)
+void modify(struct Node* root) {
+    if (!root)
+        return;
+
+    modify(root -> left);
+    modify(root -> right);
+    Node* t = root -> left;
+
+    if (t) {
+        while(t -> right) {
+            t = t -> right;
+        }
+        t -> right = root -> right;
+        root -> right = root -> left;
+    }
+}
+
+void printpre(struct Node* root) {
+    while (root != NULL) {
+        std::cout << root->data << " ";
+        root = root->right;
+    }
+}
+
+int main() {
+    struct Node* root = newNode(10);
+    root->left = newNode(8);
+    root->right = newNode(2);
+    root->left->left = newNode(3);
+    root->left->right = newNode(5);
+ 
+    modify(root);
+    printpre(root);
+	return 0;
+}
