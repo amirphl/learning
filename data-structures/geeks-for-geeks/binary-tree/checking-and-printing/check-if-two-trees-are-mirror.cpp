f606a77c693d69f848a17126fd585d06f7d79fb0
amirphl <amirphl4@gmail.com>
2022-04-25 02:38:16 +0430

    check-if-two-trees-are-mirror

---
+++ data-structures/geeks-for-geeks/binary-tree/checking-and-printing/check-if-two-trees-are-mirror.cpp
@@ -0,0 +1,93 @@
+// https://www.geeksforgeeks.org/iterative-method-check-two-trees-mirror/
+// https://www.geeksforgeeks.org/check-if-two-trees-are-mirror/
+
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int checkMirrorTree(int n, int e, int A[], int B[]) {
+        multimap<int, int> m1;
+        multimap<int, int> m2;
+
+        for(int i = 0; i < 2 * e; i += 2) {
+            m1.insert(pair<int, int> (A[i], A[i + 1]));
+            m2.insert(pair<int, int> (B[i], B[i + 1]));
+        }
+
+        // assume that 1 is root.
+        queue<int> q1;
+        queue<int> q2;
+        stack<int> s2;
+        q1.push(1);
+        q2.push(1);
+
+        while(!q1.empty() && !q2.empty()) {
+            int t1 = q1.front();
+            int t2 = q2.front();
+            q1.pop();
+            q2.pop();
+            // cout << t1 << " " << t2 << endl;
+
+            if (t1 != t2) {
+                return false;
+            }
+
+            auto itr1 = m1.lower_bound(t1);
+            auto itr2 = m1.upper_bound(t1);
+
+            while (itr1 != itr2) {
+                if (itr1 -> first == t1) {
+                    // cout << "-- " << itr1 -> first << " " << itr1 -> second << endl;
+                    q1.push(itr1 -> second);
+                }
+                itr1++;
+            }
+
+            itr1 = m2.lower_bound(t2);
+            itr2 = m2.upper_bound(t2);
+
+            while (itr1 != itr2) {
+                if (itr1 -> first == t2) {
+                    s2.push(itr1 -> second);
+                }
+                itr1++;
+            }
+
+            while(!s2.empty()) {
+                q2.push(s2.top());
+                s2.pop();
+            }
+        }
+
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,e;
+        
+        cin>>n>>e;
+        int A[2*e], B[2*e];
+        
+        for(int i=0; i<2*e; i++)
+            cin>>A[i];
+            
+        for(int i=0; i<2*e; i++)
+            cin>>B[i];
+
+        Solution ob;
+        cout << ob.checkMirrorTree(n,e,A,B) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
