5ba48e31b60754b9c0f84d46bc2e7874e4da616d
amirphl <amirphl4@gmail.com>
2022-05-15 19:30:12 +0430

    add method to find all pairs

--- data-structures/geeks-for-geeks/array/searching/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x.cpp
+++ data-structures/geeks-for-geeks/array/searching/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x.cpp
@@ -1,8 +1,11 @@
 // https://www.geeksforgeeks.org/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/
-// TODO(amirphl) remainder approach
-// TODO(amirphl) https://www.geeksforgeeks.org/given-two-unsorted-arrays-find-pairs-whose-sum-x/
-// TODO(amirphl) https://www.geeksforgeeks.org/count-pairs-with-given-sum/
-// TODO(amirphl) https://www.geeksforgeeks.org/count-pairs-difference-equal-k/
+// https://www.geeksforgeeks.org/given-two-unsorted-arrays-find-pairs-whose-sum-x/
+// https://www.geeksforgeeks.org/count-pairs-with-given-sum/
+//
+// other approaches:
+// method 1: Using remainders of the elements less than x: time: O(n + x), memory: O(x)
+// notes:
+// To handle the duplicates, use hashmap of frequencies.
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
@@ -35,11 +38,11 @@ public:
  	    return false;
 	}
 
-    // set
-    // time: O(nlogn), memory: O(n)
+    // approach: using set
+    // time: O(n), memory: O(n)
     // You can also store index of the elements by using a Map instead of a set.
     bool hasArrayTwoCandidates(int arr[], int n, int x) {
-        std::set<int> m;
+        std::unordered_set<int> m;
         int i = -1;
         while(++i < n) {
             if (m.find(x - arr[i]) == m.end())
@@ -48,6 +51,24 @@ public:
                 return true;
         }
         return false;
+    }
+
+    // to find all pairs (A has distinct elements, B has distinct elements also):
+    // time: O(nlogn), memory: O(n)
+    // notes:
+    // A and B are not sorted.
+    // The output is sorted by the first element of the pair. The time complexity can be reduced to O(n) if ignoring the sorted output.
+    // More info: https://practice.geeksforgeeks.org/problems/find-all-pairs-whose-sum-is-x5808/1/
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X) {
+        vector<pair<int,int>> res;
+        set<int> s1(A, A + N);
+        unordered_set<int> s2(B, B + M);
+        for(auto &it: s1) {
+            if (s2.find(X - it) != s2.end()) {
+                res.push_back({it, X - it});
+            }
+        }
+        return res;
     }
 };
 
