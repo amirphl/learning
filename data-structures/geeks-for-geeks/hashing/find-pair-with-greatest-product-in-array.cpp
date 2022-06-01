b64b7b90fdbe5c2957a3069efacf24fedf730aa8
amirphl <amirphl4@gmail.com>
2022-06-01 16:12:26 +0430

    add O(n*sqrt(max(arr))) solution

--- data-structures/geeks-for-geeks/hashing/find-pair-with-greatest-product-in-array.cpp
+++ data-structures/geeks-for-geeks/hashing/find-pair-with-greatest-product-in-array.cpp
@@ -34,6 +34,69 @@ class Solution{
 
         return -1;
     }
+
+        // 10
+    // 13 23 1 15 34 35 8 24 32 13
+    
+    // 9
+    // 8 31 3 5 31 6 2 32 19
+
+    // 11
+    // 4 10 4 19 28 8 23 17 1 21 7
+
+    // time: O(n * sqrt(max(arr))), memory: O(n)
+    int findGreatest2(int arr[], int n)
+    {
+        unordered_map<int, int> freq;
+        int i = 0;
+        while(i < n) {
+            freq[arr[i]]++;
+            i++;
+        }
+        int x;
+        int res = INT_MIN;
+        for(auto& z: freq) {
+            x = z.first;
+            if (x <= res) {
+                continue;
+            }
+            bool found = false;
+            for (i = 1; i * i < x; i++) {
+                if (x % i == 0 &&
+                        freq.find(x / i) != freq.end() &&
+                        freq.find(i) != freq.end() &&
+                        (x / i != i || freq[i] > 1)) {
+                    if (i == 1 && freq[x] == 1) {
+                        continue;
+                    }
+                    res = x;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                continue;
+            }
+            if (i - (x / i) == 1) {
+                i--;
+            }
+            for (; i >= 1; i--) {
+                if (x % i == 0 &&
+                        freq.find(x / i) != freq.end() &&
+                        freq.find(i) != freq.end() &&
+                        (x / i != i || freq[i] > 1)) {
+                    if (i == 1 && freq[x] == 1) {
+                        continue;
+                    }
+                    res = x;
+                    found = true;
+                    break;
+                }
+            }
+            // cout << x << " _ " << endl;
+        }
+        return res == INT_MIN ? -1 : res;
+    }
 };
 
 // { Driver Code Starts.
