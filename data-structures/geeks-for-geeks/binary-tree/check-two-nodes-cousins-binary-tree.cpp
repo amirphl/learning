5b7ae19e07f78cbdb72a3656a8864fc2a2953277
amirphl <amirphl4@gmail.com>
2022-06-09 03:37:17 +0430

    check-two-nodes-cousins-binary-tree

--- data-structures/geeks-for-geeks/binary-tree/check-two-nodes-cousins-binary-tree.cpp
+++ data-structures/geeks-for-geeks/binary-tree/check-two-nodes-cousins-binary-tree.cpp
@@ -16,83 +16,83 @@ struct Node {
 };
 // Function to Build Tree
 Node* buildTree(string str)
-{   
+{
     // Corner Case
     if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
+        return NULL;
+
+    // Creating vector of strings from input
     // string after spliting by space
     vector<string> ip;
-    
+
     istringstream iss(str);
     for(string str; iss >> str; )
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = new Node(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
     while(!queue.empty() && i < ip.size()) {
-            
+
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
         if(currVal != "N") {
-                
+
             // Create the left child for the current node
             currNode->left = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
         if(i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
         if(currVal != "N") {
-                
+
             // Create the right child for the current node
             currNode->right = new Node(stoi(currVal));
-                
+
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
 
-bool isCousins(Node*root , int x , int y);
+bool isCousins(Node*root, int x, int y);
 
 int main()
 {
 
     int t;
-	scanf("%d ",&t);
+    scanf("%d ",&t);
     while(t--)
     {
         string s;
-		getline(cin,s);
+        getline(cin,s);
         Node* root = buildTree(s);
-        int x , y;
+        int x, y;
         scanf("%d ",&x);
-		scanf("%d ",&y);
+        scanf("%d ",&y);
         if(x==y)
         {
             cout<<"0\n";
@@ -135,7 +135,7 @@ void preorder(Node* root) {
 }
 
 // Returns true if the nodes with values 'a' and 'b' are cousins. Else returns false
-// time: O(V), memory: O(V)
+// time: O(n), memory: O(n)
 bool isCousins(Node *root, int a, int b)
 {
     // inorder(root);
