16084b73f931a21832a3fc76849e4fee67e88c50
amirphl <amirphl4@gmail.com>
2022-06-08 14:23:46 +0430

    inorder-succesor-node-binary-tree

--- data-structures/geeks-for-geeks/binary-tree/inorder-succesor-node-binary-tree.cpp
+++ data-structures/geeks-for-geeks/binary-tree/inorder-succesor-node-binary-tree.cpp
@@ -4,18 +4,18 @@
 #include<iostream>
 
 class Node {
-	public:
-	int data;
-	Node* left;
-	Node* right;
+public:
+    int data;
+    Node* left;
+    Node* right;
 
-	Node(int d) {
-		data = d;
-		left = right = NULL;
-	}
+    Node(int d) {
+        data = d;
+        left = right = NULL;
+    }
 };
 
-// time: O(V), memory: O(V)
+// time: O(n), memory: O(n)
 void find(Node* root, Node* u) {
     if (u -> right) {
         Node* temp = u;
@@ -82,19 +82,19 @@ void find(Node* root, Node* u) {
 }
 
 int main() {
-	Node* root = new Node(1);
-	root -> left = new Node(2);
-	root -> left -> left = new Node(4);
-	root -> left -> right = new Node(5);
-	root -> left -> right -> left = new Node(8);
-	root -> left -> right -> left -> left = new Node(9);
-	root -> left -> right -> left -> right = new Node(10);
-	root -> right = new Node(3);
-	root -> right -> left = new Node(6);
-	root -> right -> left -> right = new Node(3);
-	root -> right -> right = new Node(7);
-	root -> right -> right -> right = new Node(11);
-	root -> right -> right -> right -> left = new Node(12);
+    Node* root = new Node(1);
+    root -> left = new Node(2);
+    root -> left -> left = new Node(4);
+    root -> left -> right = new Node(5);
+    root -> left -> right -> left = new Node(8);
+    root -> left -> right -> left -> left = new Node(9);
+    root -> left -> right -> left -> right = new Node(10);
+    root -> right = new Node(3);
+    root -> right -> left = new Node(6);
+    root -> right -> left -> right = new Node(3);
+    root -> right -> right = new Node(7);
+    root -> right -> right -> right = new Node(11);
+    root -> right -> right -> right -> left = new Node(12);
 
     std::queue<Node*> q;
     q.push(root);
