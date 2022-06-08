a6cafd7a396be4dc68552562686b16f4d6db12ef
amirphl <amirphl4@gmail.com>
2022-06-08 18:50:56 +0430

    construct-tree-from-given-inorder-and-preorder-traversal

--- data-structures/geeks-for-geeks/binary-tree/construct-tree-from-given-inorder-and-preorder-traversal.cpp
+++ data-structures/geeks-for-geeks/binary-tree/construct-tree-from-given-inorder-and-preorder-traversal.cpp
@@ -1,7 +1,5 @@
 // https://www.geeksforgeeks.org/construct-tree-from-given-inorder-and-preorder-traversal/
 
-// TODO(amirphl) second approach
-
 // { Driver Code Starts
 //
 
@@ -10,28 +8,28 @@ using namespace std;
 
 struct Node
 {
-	int data;
-	struct Node *left;
-	struct Node *right;
-	
-	Node(int x){
-	    data = x;
-	    left = NULL;
-	    right = NULL;
-	}
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
 };
 
 
 void printPostOrder(Node *root)
 {
-	if(root==NULL)
-		return;
-	printPostOrder(root->left);
-	printPostOrder(root->right);
-	cout<<root->data<<" ";
+    if(root==NULL)
+        return;
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    cout<<root->data<<" ";
 }
 
- // } Driver Code Ends
+// } Driver Code Ends
 
 
 /*Complete the code here.
@@ -43,18 +41,18 @@ struct Node
   Node* right;
 };
 */
-class Solution{
-    public:
+class Solution {
+public:
     Node* buildTree(int in[],int pre[], int n)
     {
-        std::map<int, int> m;
+        std::unordered_map<int, int> m;
         for(int i = 0; i < n; i++)
             m[in[i]] = i;
         return buildTreeRecu(in, pre, 0, n - 1, 0, n -1, m);
     }
 
-    // time: O(VlogV), memory: O(V)
-    Node* buildTreeRecu(int in[], int pre[], int low, int high, int left, int right, std::map<int, int> &m) {
+    // time: O(n), memory: O(h)
+    Node* buildTreeRecu(int in[], int pre[], int low, int high, int left, int right, std::unordered_map<int, int> &m) {
         if (low > high)
             return 0;
 
@@ -75,22 +73,22 @@ class Solution{
 // { Driver Code Starts.
 int main()
 {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		
-		int inorder[n], preorder[n];
-		for(int i=0; i<n; i++)
-			cin>> inorder[i];
-		for(int i=0; i<n; i++)
-			cin>> preorder[i];
-		Solution obj;
-		Node *root = obj.buildTree(inorder, preorder, n);
-		printPostOrder(root);
-		cout<< endl;
-	}
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int inorder[n], preorder[n];
+        for(int i=0; i<n; i++)
+            cin>> inorder[i];
+        for(int i=0; i<n; i++)
+            cin>> preorder[i];
+        Solution obj;
+        Node *root = obj.buildTree(inorder, preorder, n);
+        printPostOrder(root);
+        cout<< endl;
+    }
 }
-  // } Driver Code Ends
+// } Driver Code Ends
