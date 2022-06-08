a0967647ccffb981169a1b02e61c8596266297d8
amirphl <amirphl4@gmail.com>
2022-06-09 04:08:13 +0430

    check-weather-given-binary-tree-perfect-not

---
+++ data-structures/geeks-for-geeks/binary-tree/check-weather-given-binary-tree-perfect-not.cpp
@@ -0,0 +1,177 @@
+// https://www.geeksforgeeks.org/check-weather-given-binary-tree-perfect-not/
+
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    // We can also step at the first level found that the tree is not prefect.
+    // time: O(V), memory: O(V)
+    bool isPerfect(Node *root)
+    {
+        queue<Node*> q;
+        q.push(root);
+        q.push(0);
+        int num = 1;
+        int p = 0;
+
+        while(q.size() > 1) {
+            Node* u = q.front();
+            q.pop();
+
+            if (u == 0) {
+                p++;
+                num = pow(2, p);
+                q.push(0);
+                continue;
+            }
+
+            num--;
+
+            if (u -> left) {
+                q.push(u -> left);
+            }
+            if (u -> right) {
+                q.push(u -> right);
+            }
+        }
+
+        return num == 0;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        bool res = obj.isPerfect(root);
+        if (res)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}  // } Driver Code Ends
