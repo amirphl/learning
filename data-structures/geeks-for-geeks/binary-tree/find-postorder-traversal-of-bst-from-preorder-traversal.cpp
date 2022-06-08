b5b7a8788c11b9cde3775dde1581527a3cc6c559
amirphl <amirphl4@gmail.com>
2022-06-08 13:55:16 +0430

    find-postorder-traversal-of-bst-from-preorder-traversal

--- data-structures/geeks-for-geeks/binary-tree/find-postorder-traversal-of-bst-from-preorder-traversal.cpp
+++ data-structures/geeks-for-geeks/binary-tree/find-postorder-traversal-of-bst-from-preorder-traversal.cpp
@@ -1,14 +1,10 @@
 // https://www.geeksforgeeks.org/find-postorder-traversal-of-bst-from-preorder-traversal/
 
-// other approches:
-// TODO(amirphl) There is a better approche with time = O(V)
-
 #include<bits/stdc++.h>
 #include<iostream>
 
-// time: O(V), memory: O(1)
-// assumed that finding in map takes O(1)
-void recur(int in[], int pre[], int in_l, int in_r, int pre_l, int pre_r, std::map<int, int> &m)
+// time: O(n), memory: O(h)
+void recur(int in[], int pre[], int in_l, int in_r, int pre_l, int pre_r, std::unordered_map<int, int> &m)
 {
     int root_index = m[pre[pre_l]];
     if (root_index > in_l) {
@@ -22,24 +18,25 @@ void recur(int in[], int pre[], int in_l, int in_r, int pre_l, int pre_r, std::m
 
 void printPostOrder(int in[], int pre[], int n)
 {
-    std::map<int, int> m;
+    std::unordered_map<int, int> m;
     for(int i = 0; i < n; i++)
         m[in[i]] = i;
     recur(in, pre, 0, n -1, 0, n - 1, m);
 }
 
-// time: O(VlogV), memory: O(V)
+// time: O(nlogn), memory: O(n)
 void find_postorder_bst(int pre[], int n) {
-	int in[n];
-	memcpy(in, pre, n * sizeof(int));
-	std::sort(in, in + n);
-	printPostOrder(in, pre, n);
+    int in[n];
+    memcpy(in, pre, n * sizeof(int));
+    std::sort(in, in + n);
+    printPostOrder(in, pre, n);
 }
 
 int main() {
-	// int pre[] = {40, 30, 35, 80, 100};
-	int pre[] = {40, 30, 32, 35, 80, 90, 100, 120};
-	int n = sizeof(pre) / sizeof(pre[0]);
-	find_postorder_bst(pre, n);
-	return 0;
+    // int pre[] = {40, 30, 35, 80, 100};
+    int pre[] = {40, 30, 32, 35, 80, 90, 100, 120};
+    int n = sizeof(pre) / sizeof(pre[0]);
+    find_postorder_bst(pre, n);
+    std::cout << std::endl;
+    return 0;
 }
