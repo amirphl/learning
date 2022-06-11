c56bc3ef4d9eecf19346c469dcbebcfd56a379c0
amirphl <amirphl4@gmail.com>
2022-06-11 15:13:20 +0430

    diagonal-sum-binary-tree

---
+++ data-structures/geeks-for-geeks/binary-tree/diagonal-sum-binary-tree.cpp
@@ -0,0 +1,145 @@
+// https://www.geeksforgeeks.org/diagonal-sum-binary-tree/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+vector<int> diagonalSum(Node*root);
+
+int main()
+{
+
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        vector <int> res = diagonalSum(root);
+        for (int i : res) cout << i << " ";
+        cout<<endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node{
+    int data;
+    Node *left,*right;
+};
+*/
+// time: O(n), memory: O(n)
+vector <int> diagonalSum(Node* root) {
+    Node* u = root;
+    vector<int> vec;
+    queue<Node*> q;
+    q.push(root);
+    q.push(0);
+    int s = 0;
+
+    while(q.size() > 1) {
+        u = q.front();
+        q.pop();
+
+        if (u == 0) {
+            vec.push_back(s);
+            s = 0;
+            q.push(0);
+            continue;
+        }
+
+        while(u) {
+            s += u -> data;
+            if (u -> left) {
+                q.push(u -> left);
+            }
+            u = u -> right;
+        }
+    }
+
+    vec.push_back(s);
+    return vec;
+}
