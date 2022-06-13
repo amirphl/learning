1a4e85017e6f5713dd12317f4451b56f733e3d07
amirphl <amirphl4@gmail.com>
2022-06-13 20:05:18 +0430

    given-a-binary-tree-how-do-you-remove-all-the-half-nodes

---
+++ data-structures/geeks-for-geeks/binary-tree/given-a-binary-tree-how-do-you-remove-all-the-half-nodes.cpp
@@ -0,0 +1,151 @@
+// https://www.geeksforgeeks.org/given-a-binary-tree-how-do-you-remove-all-the-half-nodes/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+void inorder(Node * node)
+{
+    if(node==NULL)
+        return;
+
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
+}
+Node* RemoveHalfNodes(Node* root) ;
+
+int main()
+{
+
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Node * fresh = RemoveHalfNodes(root);
+        inorder(fresh);
+        cout<<endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// time: O(n), memory: O(n)
+Node* RemoveHalfNodesRec(Node *root) {
+    if (!root) {
+        return 0;
+    }
+    bool flag = true;
+    if ((root -> left && root -> right) || (root -> left == 0 && root -> right == 0)) {
+        flag = false;
+    }
+    root -> left = RemoveHalfNodesRec(root -> left);
+    root -> right = RemoveHalfNodesRec(root -> right);
+    if (flag) {
+        Node* u = 0;
+        if (root -> left) {
+            u = root -> left;
+        } else if (root -> right) {
+            u = root -> right;
+        }
+        delete(root);
+        return u;
+    } else {
+        return root;
+    }
+}
+
+// Return the root of the modified tree after removing all the half nodes.
+Node *RemoveHalfNodes(Node *root) {
+    return RemoveHalfNodesRec(root);
+}
