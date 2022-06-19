de12c540d26a807d55acfda63014eacc08a21e37
amirphl <amirphl4@gmail.com>
2022-06-19 20:44:55 +0430

    merge-k-sorted-arrays

--- data-structures/geeks-for-geeks/heap/merge-k-sorted-arrays.cpp
+++ data-structures/geeks-for-geeks/heap/merge-k-sorted-arrays.cpp
@@ -12,18 +12,18 @@
 using namespace std;
 void printArray(vector<int> arr, int size)
 {
-for (int i=0; i < size; i++)
-	cout << arr[i] << " ";
+    for (int i=0; i < size; i++)
+        cout << arr[i] << " ";
 }
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function Template for C++
 
 
 class Solution
 {
-    public:
+public:
     //Function to merge k sorted arrays.
     // time: O(n*n*logn), memory: O(n)
     vector<int> mergeKArrays(vector<vector<int>> arr, int K)
@@ -55,23 +55,23 @@ class Solution
 
 int main()
 {
-	int t;
-	cin>>t;
-	while(t--){
-	    int k;
-	    cin>>k;
-	    vector<vector<int>> arr(k, vector<int> (k, 0));
-	    for(int i=0; i<k; i++){
-	        for(int j=0; j<k; j++)
-	        {
-	            cin>>arr[i][j];
-	        }
-	    }
-	    Solution obj;
-    	vector<int> output = obj.mergeKArrays(arr, k);
-    	printArray(output, k*k);
-    	cout<<endl;
+    int t;
+    cin>>t;
+    while(t--) {
+        int k;
+        cin>>k;
+        vector<vector<int>> arr(k, vector<int> (k, 0));
+        for(int i=0; i<k; i++) {
+            for(int j=0; j<k; j++)
+            {
+                cin>>arr[i][j];
+            }
+        }
+        Solution obj;
+        vector<int> output = obj.mergeKArrays(arr, k);
+        printArray(output, k*k);
+        cout<<endl;
     }
-	return 0;
+    return 0;
 }
-  // } Driver Code Ends
+// } Driver Code Ends
