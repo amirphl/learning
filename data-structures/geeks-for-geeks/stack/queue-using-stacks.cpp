7aa5db6c5f9575abdcd4d5de1d57fbff3f2959e7
amirphl <amirphl4@gmail.com>
2022-06-20 13:44:53 +0430

    queue-using-stacks

--- data-structures/geeks-for-geeks/stack/queue-using-stacks.cpp
+++ data-structures/geeks-for-geeks/stack/queue-using-stacks.cpp
@@ -8,7 +8,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function Template for C++
 
 class Queue {
@@ -45,23 +45,23 @@ int main()
 
         int Q;
         cin>>Q;
-        while(Q--){
-        int QueryType=0;
-        cin>>QueryType;
-        if(QueryType==1)
-        {
-            int a;
-            cin>>a;
-            ob.enqueue(a);
-        }
-        else if(QueryType==2)
-        {
-            cout<<ob.dequeue()<<" ";
+        while(Q--) {
+            int QueryType=0;
+            cin>>QueryType;
+            if(QueryType==1)
+            {
+                int a;
+                cin>>a;
+                ob.enqueue(a);
+            }
+            else if(QueryType==2)
+            {
+                cout<<ob.dequeue()<<" ";
 
+            }
         }
-        }
-    cout<<endl;
+        cout<<endl;
     }
 }
-  // } Driver Code Ends
+// } Driver Code Ends
 
