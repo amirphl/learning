d89a04b5d7f8fca834320f0b3e24eef8cb4531a6
amirphl <amirphl4@gmail.com>
2022-06-20 16:03:08 +0430

    Add solution based on stack

--- data-structures/geeks-for-geeks/stack/next-greater-element.cpp
+++ data-structures/geeks-for-geeks/stack/next-greater-element.cpp
@@ -1,23 +1,21 @@
 // https://www.geeksforgeeks.org/next-greater-element/
 // https://www.geeksforgeeks.org/next-greater-element-in-same-order-as-input/
 // https://www.geeksforgeeks.org/next-greater-frequency-element/
-// TODO(amirphl) https://www.geeksforgeeks.org/maximum-product-of-indexes-of-next-greater-on-left-and-right/
+// https://www.geeksforgeeks.org/maximum-product-of-indexes-of-next-greater-on-left-and-right/
 // https://www.geeksforgeeks.org/print-next-greater-number-q-queries/
-
-// We can also use Stack to solve the problem.
 
 // { Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
-    public:
+public:
     // time: O(n), memory: O(n)
     // How to prove it's O(n) (time)?
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
+    vector<long long> nextLargerElement2(vector<long long> arr, int n) {
         std::vector<long long> v(n);
         std::vector<long long> index(n);
         v[n - 1] = -1;
@@ -44,6 +42,32 @@ class Solution
 
         return v;
     }
+
+    // time: O(n), memory: O(n)
+    vector<long long> nextLargerElement(vector<long long> arr, int n) {
+        std::vector<long long> v(n);
+        stack<long long> s;
+        v[n - 1] = -1;
+        s.push(arr[n - 1]);
+        int i = n - 2;
+        while(i >= 0) {
+            if (arr[i] < arr[i + 1]) {
+                v[i] = arr[i + 1];
+            } else {
+                while(!s.empty() && s.top() <= arr[i]) {
+                    s.pop();
+                }
+                if (s.empty()) {
+                    v[i] = -1;
+                } else {
+                    v[i] = s.top();
+                }
+            }
+            s.push(arr[i]);
+            i--;
+        }
+        return v;
+    }
 };
 
 // { Driver Code Starts.
@@ -54,18 +78,17 @@ int main()
     cin>>t;
     while(t--)
     {
-        
+
         int n;
         cin>>n;
         vector<long long> arr(n);
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
             cin>>arr[i];
-        
+
         Solution obj;
         vector <long long> res = obj.nextLargerElement(arr, n);
         for (long long i : res) cout << i << " ";
         cout<<endl;
     }
-	return 0;
+    return 0;
 }  // } Driver Code Ends
- 
