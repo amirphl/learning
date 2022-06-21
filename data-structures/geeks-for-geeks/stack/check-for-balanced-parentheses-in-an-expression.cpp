8a32d5dd7995980af2ddbb864098d12e6a72d1d5
amirphl <amirphl4@gmail.com>
2022-06-21 13:39:54 +0430

    Add solution to support [(]) testcase

--- data-structures/geeks-for-geeks/stack/check-for-balanced-parentheses-in-an-expression.cpp
+++ data-structures/geeks-for-geeks/stack/check-for-balanced-parentheses-in-an-expression.cpp
@@ -44,3 +44,40 @@ bool valid(string s)
     }
     return v1 == 0 && v2 == 0 && v3 == 0;
 }
+
+// This solution supports strings like [(])
+// link to practice: https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1/
+class Solution
+{
+public:
+    //Function to check if brackets are balanced or not.
+    // time: O(n), memory: O(n)
+    bool ispar(string x)
+    {
+        stack<char> s;
+        int i = 0, n = x.length();
+        while(i < n) {
+            if (x[i] == ')') {
+                if (s.empty() || s.top() != '(') {
+                    return false;
+                }
+                s.pop();
+            } else if (x[i] == '}') {
+                if (s.empty() || s.top() != '{') {
+                    return false;
+                }
+                s.pop();
+            } else if (x[i] == ']') {
+                if (s.empty() || s.top() != '[') {
+                    return false;
+                }
+                s.pop();
+            } else {
+                s.push(x[i]);
+            }
+            i++;
+        }
+        return s.empty();
+    }
+
+};
