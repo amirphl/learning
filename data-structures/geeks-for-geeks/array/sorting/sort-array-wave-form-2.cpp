a1c04a46f3c43d121cf9137090bfab979714ef97
amirphl <amirphl4@gmail.com>
2022-08-18 13:17:10 +0430

    sort-array-wave-form-2

---
+++ data-structures/geeks-for-geeks/array/sorting/sort-array-wave-form-2.cpp
@@ -0,0 +1,48 @@
+// https://www.geeksforgeeks.org/sort-array-wave-form-2/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution {
+public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    // time: O(n), memory: O(1)
+    void convertToWave(int n, vector<int>& arr) {
+        int i = 0;
+        while(i < n - 1) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            i += 2;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0; i<n; i++)
+            cin>>a[i]; //input elements of array
+        Solution ob;
+        ob.convertToWave(n, a);
+
+        for(int i=0; i<n; i++)
+            cout<<a[i]<<" "; //print array
+
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
