1d04a5f3907ffa12899a3181a0d62173f8385dbd
amirphl <amirphl4@gmail.com>
2022-08-18 12:47:33 +0430

    nearly-sorted-algorithm

--- data-structures/geeks-for-geeks/array/sorting/nearly-sorted-algorithm.cpp
+++ data-structures/geeks-for-geeks/array/sorting/nearly-sorted-algorithm.cpp
@@ -4,39 +4,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 
 
-class Solution{
-	public:
-	// time: O(klogk + nlogk) = O(nlogk), memory: O(k)
-	void required(long long arr[], long long n, long long k) {
-		if (k >= n)
-			k = n - 1;
+class Solution {
+public:
+    // time: O(klogk + nlogk) = O(nlogk), memory: O(k)
+    void required(long long arr[], long long n, long long k) {
+        if (k >= n)
+            k = n - 1;
 
-		std::priority_queue<long long, std::vector<long long>, std::greater<long long>> pq;
-		for (int i = 0; i <= k; i++) {
-			pq.push(arr[i]);
-		}
+        std::priority_queue<long long, std::vector<long long>, std::greater<long long>> pq;
+        for (int i = 0; i <= k; i++) {
+            pq.push(arr[i]);
+        }
 
-		int next;
+        int next;
 
-		for (int i = 0; i < n; i++) {
-			arr[i] = pq.top();
-			pq.pop();
-			next = i + k + 1;
-			if (next < n)
-				pq.push(arr[next]);
-		}
-	}
+        for (int i = 0; i < n; i++) {
+            arr[i] = pq.top();
+            pq.pop();
+            next = i + k + 1;
+            if (next < n)
+                pq.push(arr[next]);
+        }
+    }
 
-	void print(long long arr[], int n) {
-		for (int i = 0; i < n; i++) {
-			std::cout << arr[i] << " ";
-		}
+    void print(long long arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            std::cout << arr[i] << " ";
+        }
 
-		std::cout << std::endl;
-	}
+        std::cout << std::endl;
+    }
 };
 
 // { Driver Code Starts.
