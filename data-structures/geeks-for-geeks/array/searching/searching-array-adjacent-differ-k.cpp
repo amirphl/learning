7c777feac0717cea7c98c4fdaf2b6d0541dabc22
amirphl <amirphl4@gmail.com>
2022-08-26 00:26:35 +0430

    Fix style

--- data-structures/geeks-for-geeks/array/searching/searching-array-adjacent-differ-k.cpp
+++ data-structures/geeks-for-geeks/array/searching/searching-array-adjacent-differ-k.cpp
@@ -15,20 +15,20 @@ int search(int arr[], int n, int x, int k);
 // Driver program to test above function
 int main()
 {
-	int t,n,k,x;
-	cin>>t;
-	while(t--)
+    int t,n,k,x;
+    cin>>t;
+    while(t--)
     {
 
         cin>>n>>k;
         int arr[n];
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
             cin>>arr[i];
         cin>>x;
         cout<<search(arr, n, x, k)<<endl;
 
     }
-return 0;
+    return 0;
 }
 
 // } Driver Code Ends
