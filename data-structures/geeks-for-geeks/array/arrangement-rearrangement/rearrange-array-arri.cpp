d5f89f4461cdc91927964f1cbc1ae6d7ed9d2829
amirphl <amirphl4@gmail.com>
2022-09-08 14:40:16 +0430

    Restyle

--- data-structures/geeks-for-geeks/array/arrangement-rearrangement/rearrange-array-arri.cpp
+++ data-structures/geeks-for-geeks/array/arrangement-rearrangement/rearrange-array-arri.cpp
@@ -10,27 +10,29 @@ using namespace std;
 int * Rearrange(int *arr,int n);
 
 int main() {
-	//code
-	
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n;cin>>n;
-	    int a[n];
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	    int *b = Rearrange(a,n);
-	    for(int i=0;i<n;i++)
-	        cout << b[i] <<" ";
-	    cout << endl;
-	    
-	}
-	
-	return 0;
+    //code
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; i++)
+            cin>>a[i];
+        int *b = Rearrange(a,n);
+        for(int i=0; i<n; i++)
+            cout << b[i] <<" ";
+        cout << endl;
+
+    }
+
+    return 0;
 }// } Driver Code Ends
 
 
-void put(int arr[], int b){
+void put(int arr[], int b) {
     // cout << b << " ";
     if (arr[b] == b) {
         return;
@@ -43,7 +45,7 @@ void put(int arr[], int b){
     }
 }
 
-int * Rearrange2(int *arr,int n){
+int * Rearrange2(int *arr,int n) {
     for (int i = 0; i < n; i++) {
         if (arr[i] != -1) {
             // cout << " -" << i << "- ";
