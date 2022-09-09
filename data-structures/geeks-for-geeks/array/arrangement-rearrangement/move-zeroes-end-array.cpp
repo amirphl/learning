aad596edea83629df7da9c5de0a655bf65c0cd9d
amirphl <amirphl4@gmail.com>
2022-09-09 13:04:56 +0430

    Restyle

--- data-structures/geeks-for-geeks/array/arrangement-rearrangement/move-zeroes-end-array.cpp
+++ data-structures/geeks-for-geeks/array/arrangement-rearrangement/move-zeroes-end-array.cpp
@@ -6,50 +6,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
-class Solution{
+class Solution {
 public:
     // time: O(n), memory: O(1)
-	void pushZerosToEnd(int arr[], int n) {
-	    int i = 0, j = 0;
+    void pushZerosToEnd(int arr[], int n) {
+        int i = 0, j = 0;
 
-	    while (i < n) {
-	        if (arr[i] != 0) {
-	            arr[i - j] = arr[i];
-	            i++;
-	            continue;
-	        }
+        while (i < n) {
+            if (arr[i] != 0) {
+                arr[i - j] = arr[i];
+                i++;
+                continue;
+            }
 
             i++;
-	        j++;
-	    }
+            j++;
+        }
 
-	    while (j > 0) {
-	        arr[n - j] = 0;
-	        j--;
-	    }
-	}
+        while (j > 0) {
+            arr[n - j] = 0;
+            j--;
+        }
+    }
 
     // time: O(n), memory: O(1)
-	void pushZerosToEndZeroTraversal(int arr[], int n) {
-	    int i = 0, j = 0;
+    void pushZerosToEndZeroTraversal(int arr[], int n) {
+        int i = 0, j = 0;
 
-	    while (i < n) {
-	        if (arr[i] != 0) {
-	            arr[i - j] = arr[i];
+        while (i < n) {
+            if (arr[i] != 0) {
+                arr[i - j] = arr[i];
 
-	            if (j != 0)
-	                arr[i] = 0;
+                if (j != 0)
+                    arr[i] = 0;
 
-	            i++;
-	            continue;
-	        }
+                i++;
+                continue;
+            }
 
             i++;
-	        j++;
-	    }
-	}
+            j++;
+        }
+    }
 };
 
 // { Driver Code Starts.
