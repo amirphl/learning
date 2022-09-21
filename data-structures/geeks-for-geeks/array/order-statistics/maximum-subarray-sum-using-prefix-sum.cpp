311085452a7a1a8db0d9ad64c3f36c798097a537
amirphl <amirphl4@gmail.com>
2022-09-21 23:15:24 +0430

    maximum-subarray-sum-using-prefix-sum

--- data-structures/geeks-for-geeks/array/order-statistics/maximum-subarray-sum-using-prefix-sum.cpp
+++ data-structures/geeks-for-geeks/array/order-statistics/maximum-subarray-sum-using-prefix-sum.cpp
@@ -1,36 +1,58 @@
 // https://www.geeksforgeeks.org/maximum-subarray-sum-using-prefix-sum/
 
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
-// time: O(n), memory: O(1)
-void find(int arr[], int n) {
-    int pre[n];
-    pre[0] = arr[0];
 
-    for(int i = 1; i < n; i++) {
-        pre[i] = pre[i - 1] + arr[i];
+// } Driver Code Ends
+class Solution {
+public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    // time: O(n), memory: O(n)
+    long long maxSubarraySum(int arr[], int n) {
+        int pre_sum[n];
+        int i = 0, s = 0;
+        while(i < n) {
+            s += arr[i];
+            pre_sum[i] = s;
+            i++;
+        }
+        int res = arr[0];
+        int min_pre_sum = arr[0];
+        i = 1;
+        while(i < n) {
+            res = max({res, pre_sum[i], pre_sum[i] - min_pre_sum});
+            min_pre_sum = min(min_pre_sum, pre_sum[i]);
+            i++;
+        }
+        return res;
     }
+};
 
-    int min_pre_so_far = pre[0];
-    int max_so_far = arr[0];
+//{ Driver Code Starts.
 
-    for(int i = 1; i < n; i++) {
-        max_so_far = max(pre[i] - min_pre_so_far, max_so_far);
-        min_pre_so_far = min(pre[i], min_pre_so_far);
+int main()
+{
+    int t,n;
+
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+
+        cin>>n; //input size of array
+
+        int a[n];
+
+        for(int i=0; i<n; i++)
+            cin>>a[i]; //inputting elements of array
+
+        Solution ob;
+
+        cout << ob.maxSubarraySum(a, n) << endl;
     }
-    cout << max_so_far << endl;
 }
 
-int main() {
-    int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    find(arr, n);
-
-    int arr2[] = { 4, -8, 9, -4, 1, -8, -1, 6 };
-    int n2 = sizeof(arr2) / sizeof(arr2[0]);
-
-    find(arr2, n2);
-
-    return 0;
-}
+// } Driver Code Ends
