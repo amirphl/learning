76e75850002fc6b1aaf612a450eb769812641e0c
amirphl <amirphl4@gmail.com>
2022-09-21 23:34:39 +0330

    Reformat

--- data-structures/geeks-for-geeks/array/order-statistics/k-smallest-elements-order-using-o1-extra-space.cpp
+++ data-structures/geeks-for-geeks/array/order-statistics/k-smallest-elements-order-using-o1-extra-space.cpp
@@ -4,54 +4,54 @@
 #include<iostream>
 
 void fill_arr(int arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		arr[i] =  (rand() % 100);
-	}
+    for (int i = 0; i < n; i++) {
+        arr[i] =  (rand() % 100);
+    }
 }
 
 void print(int arr[], int n) {
-	for (int i = 0; i < n; i++) {
-		std::cout << arr[i] << " ";
-	}
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 }
 
 // time: O((n - k)*n), memory: O(1)
-void find_kth(int arr[], int n , int k) {
-	int max_ind = 0;
+void find_kth(int arr[], int n, int k) {
+    int max_ind = 0;
 
-	for (int i = 0; i < k; i++)
-		if (arr[i] > arr[max_ind])
-			max_ind = i;
+    for (int i = 0; i < k; i++)
+        if (arr[i] > arr[max_ind])
+            max_ind = i;
 
-	for (int i = k; i < n; i++) {
-		if (arr[i] >= arr[max_ind])
-			continue;
+    for (int i = k; i < n; i++) {
+        if (arr[i] >= arr[max_ind])
+            continue;
 
-		int temp = arr[i];
-		arr[i] = arr[max_ind];
+        int temp = arr[i];
+        arr[i] = arr[max_ind];
 
-		for (int j = max_ind; j < k - 1; j++)
-			arr[j] = arr[j + 1];
+        for (int j = max_ind; j < k - 1; j++)
+            arr[j] = arr[j + 1];
 
-		arr[k - 1] = temp;
+        arr[k - 1] = temp;
 
-		for (int i = 0; i < k; i++)
-			if (arr[i] > arr[max_ind])
-				max_ind = i;
-	}
+        for (int i = 0; i < k; i++)
+            if (arr[i] > arr[max_ind])
+                max_ind = i;
+    }
 
-	print(arr, n);
+    print(arr, n);
 }
 
 int main() {
-	int arr[40];
-	int n = sizeof(arr) / sizeof(arr[0]);
-	fill_arr(arr, n);
-	print(arr, n);
-	find_kth(arr, n, 10);
-	// std::sort(arr, arr + n);
-	// print(arr, n);
-	return 0;
+    int arr[40];
+    int n = sizeof(arr) / sizeof(arr[0]);
+    fill_arr(arr, n);
+    print(arr, n);
+    find_kth(arr, n, 10);
+    // std::sort(arr, arr + n);
+    // print(arr, n);
+    return 0;
 }
