4f60115bf5f30f3d96e03031e04d54be4d58fe3c
amirphl <amirphl4@gmail.com>
2022-09-22 13:55:12 +0330

    Reformat

--- data-structures/geeks-for-geeks/array/order-statistics/maximum-sum-such-that-no-two-elements-are-adjacent.cpp
+++ data-structures/geeks-for-geeks/array/order-statistics/maximum-sum-such-that-no-two-elements-are-adjacent.cpp
@@ -1,5 +1,5 @@
 // https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
-// TODO(amirphl) https://www.geeksforgeeks.org/find-maximum-possible-stolen-value-houses/
+// https://www.geeksforgeeks.org/find-maximum-possible-stolen-value-houses/
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
@@ -7,31 +7,31 @@
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
-class Solution{
-public:	
-	// calculate the maximum sum with out adjacent
-	// time: O(n), memory: O(n) - can easily reduce the memory to O(1)
-	int findMaxSum(int *arr, int n) {
-	    if (n == 1)
-	        return arr[0];
+class Solution {
+public:
+    // calculate the maximum sum with out adjacent
+    // time: O(n), memory: O(n) - can easily reduce the memory to O(1)
+    int findMaxSum(int *arr, int n) {
+        if (n == 1)
+            return arr[0];
 
-	    int dp[n];
-	    dp[0] = arr[0];
-	    dp[1] = std::max(arr[0], arr[1]);
-	    int s, u;
+        int dp[n];
+        dp[0] = arr[0];
+        dp[1] = std::max(arr[0], arr[1]);
+        int s, u;
 
-	    for (int i = 2; i < n; i++) {
-	        if (dp[i - 1] == dp[i - 2]) {
+        for (int i = 2; i < n; i++) {
+            if (dp[i - 1] == dp[i - 2]) {
                 dp[i] = arr[i] + dp[i - 1];
-	        } else {
-	            dp[i] = std::max(dp[i - 1], arr[i] + dp[i - 2]);
-	        }
-	    }
-	    
-	    return dp[n - 1];
-	}
+            } else {
+                dp[i] = std::max(dp[i - 1], arr[i] + dp[i - 2]);
+            }
+        }
+
+        return dp[n - 1];
+    }
 };
 
 // { Driver Code Starts.
