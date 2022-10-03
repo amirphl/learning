bc4c1d58145d5a21f74169a24bb06c59eb596d86
amirphl <amirphl4@gmail.com>
2022-10-03 18:35:52 +0330

    tilt-binary-tree

---
+++ data-structures/geeks-for-geeks/binary-tree/tilt-binary-tree.cpp
@@ -0,0 +1,163 @@
+// https://www.geeksforgeeks.org/tilt-binary-tree/
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+/*
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    // Your are required to complete this function
+    // function should return the tilt of the tree
+    // time: O(n), memory: O(h)
+    int tiltTree(Node* root)
+    {
+        int res = 0;
+        int nodes_sum = 0;
+        tiltTreeRec(root, res, nodes_sum);
+        return res;
+    }
+
+    void tiltTreeRec(Node* root, int& res, int& nodes_sum)
+    {
+        if (!root) {
+            nodes_sum = 0;
+            return;
+        }
+        int left_sum = 0;
+        int right_sum = 0;
+        tiltTreeRec(root -> left, res, left_sum);
+        tiltTreeRec(root -> right, res, right_sum);
+        res += abs(left_sum - right_sum);
+        nodes_sum = left_sum + right_sum + root -> data;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int res = obj.tiltTree(root);
+        cout << res << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
