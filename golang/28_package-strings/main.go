1255c038edc610f44de6d1c537892e55552b2ae6
amirphl <amirphl4@gmail.com>
2022-10-16 18:20:31 +0330

    strings

---
+++ golang/28_package-strings/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// strings Contains, Count, HasPrefix, HasSuffix, Index, Join, Repeat, Replace, Split, ToLower, ToUpper, TrimSpace
+
+func main() {
+	fmt.Println(strings.Replace("salamsalam", "a", "hi", 2))
+	fmt.Println(string([]rune{'g', 'o', 'o', 'd'}))
+}
