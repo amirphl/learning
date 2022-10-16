5218a0389113439c42d762d0648000aa2e51df5e
amirphl <amirphl4@gmail.com>
2022-10-16 18:57:53 +0330

    bufio

---
+++ golang/29_package-bufio/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// br := bufio.NewReader(src)
+// _, err := io.Copy(dst, br)
+
+// scanner := bufio.NewScanner(src)
+// for scanner.Scan() {
+//    line := scanner.Text()
+// }
+
+// scanner := bufio.NewScanner(os.Stdin)
+// if err := scanner.Err(); err != nil {}
+// fmt.Fprintln(os.Stderr, "reading standard input:", err)
+
+// scanner := bufio.NewScanner(src)
+// scanner.Split(bufio.ScanWords)
+
+// scanner := bufio.NewScanner(strings.NewReader(input))
+
+func main() {
+	fmt.Println("vim-go")
+}
