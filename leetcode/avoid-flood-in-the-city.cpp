5a054ae2ce2989782009ec6fa96aa46f8fcbc0ed
amirphl <amirphl4@gmail.com>
2023-08-23 23:51:27 +0330

    avoid-flood-in-the-city

---
+++ leetcode/avoid-flood-in-the-city.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/avoid-flood-in-the-city/
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+  // TLE
+  vector<int> avoidFlood(vector<int> &rains) {
+    int n = rains.size();
+    vector<int> res(n, 1);
+    set<int> t;
+    unordered_map<int, int> s;
+    for (int i = 0; i < n; i++) {
+      int r = rains[i];
+      if (r) {
+        res[i] = -1;
+        if (s.find(r) == s.end()) {
+          s[r] = i;
+        } else {
+          int d = s[r];
+          auto it = lower_bound(t.begin(), t.end(), d);
+          if (it == t.end()) {
+            return {};
+          }
+          res[*it] = r;
+          s[r] = i;
+          t.erase(it);
+        }
+      } else {
+        t.insert(i);
+      }
+    }
+    return res;
+  }
+};
